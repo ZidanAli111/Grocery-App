71d3b3be57b93915571d80244fab82c373399bfa
ZidanAli111 <zidanali111@gmail.com>
2021-11-14 19:20:06 +0530

    Initial Commit

--- app/src/main/java/android/example/groceryapp/MainActivity.java
+++ app/src/main/java/android/example/groceryapp/MainActivity.java
@@ -2,7 +2,12 @@ package android.example.groceryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton floatingActionButton=findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ListView productView=(ListView) findViewById(R.id.list);
+        View emptyView=findViewById(R.id.empty_view);
+
+        productView.setEmptyView(emptyView);
+
+
+
     }
 }
